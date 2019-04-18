b2b104e05ec7edcdbb8d8b2401e6753afbdec2e8
52757 <527573174@qq.com>
2019-04-18 18:21:03 +0800

    add npy file

--- utils/npy.py
+++ utils/npy.py
@@ -3,21 +3,37 @@ import json
 import pandas as pd
 import pycocotools.mask as mask_util
 import numpy as np
+import os
+from progressbar import *
 
-path = r'./train/restricted'
 
-with open(path + 'result0418am.json', 'r') as f:
-    result = json.load(f)
-f.close()
+def make_npy():
+    path = r'../data/train/predict_npy'
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-classes = pd.read_csv('./classifier-train-a.csv', index_col=False)
+    with open('../result0418am.json', 'r') as f:
+        result = json.load(f)
+    f.close()
 
-for i in range(len(classes)):
+    widgets = ['Progress: ', Percentage(), ' ', Bar('#'), ' ', Timer(),
+               ' ', ETA(), ' ']
+    pbar = ProgressBar(widgets=widgets, maxval=len(result.keys())).start()
+    i = 0
+    for key, value in result.items():
+        i = i + 1
+        pbar.update(i)
+        masks = result[key]['mask']
+        height, width = result[key]['size']
+        save_path = os.path.join(path, str(key).strip('.jpg'))
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+        for idx in range(1, 6):
+            # a = np.zeros((height,width),dtype=np.uint8)
+            arr = mask_util.decode(masks[str(idx)])
 
-    masks = result[classes['name'][i]]['mask']
-    height, width = result[classes['name'][i]]['size']
-    for idx in range(1, 6):
-        a = [[0 for _ in range(width)] for _ in range(height)]
-        a = np.array(a, dtype=np.uint8)
-        arr = mask_util.decode(masks[str(idx)]['counts'])
-        arr  # 这个就是那个矩阵了
+            np.save(os.path.join(save_path, r"{}.npy".format(idx)), arr)  # 这个就是那个矩阵了
+
+
+if __name__ == '__main__':
+    make_npy()
