221d817eb32208ab7ee085146778276082fd44b8
52757 <527573174@qq.com>
2019-04-22 11:44:19 +0800

    add data vertical

--- utils/AuxiliaryTool.py
+++ utils/AuxiliaryTool.py
@@ -2,6 +2,8 @@
 import os
 from PIL import Image
 import numpy as np
+import json
+from progressbar import *
 IMG_SIZE = 640
 
 def img_mean_std(inDir):
@@ -64,3 +66,72 @@ def img_mean_std(inDir):
 
     RGB_std = [R_std,G_std,B_std]
     return RGB_mean,RGB_std
+
+
+def ImgVertical():
+    path = r'../data/train'
+    with open(os.path.join(path, 'train_restriction.json'), 'r') as f:
+        annos = json.load(f)
+    f.close()
+
+    img_path = path + r'/restricted'
+    img_names = os.listdir(img_path)
+
+    imageInfo = annos['images']
+    imageInfo = sorted(imageInfo, key=lambda x: int(x['file_name'].strip('.jpg')))
+    img_nums = len(imageInfo)
+
+    imageAnnos = annos['annotations']
+    imageAnnos = sorted(imageAnnos, key=lambda x: x['id'])
+    annos_nums = len(imageAnnos)
+    idx = 0
+
+    widgets = ['Progress: ', Percentage(), ' ', Bar('#'), ' ', Timer(),
+               ' ', ETA(), ' ']
+    pbar = ProgressBar(widgets=widgets, maxval=img_nums).start()
+
+    for i in range(img_nums):
+        pbar.update(i)
+
+        info_tmp = imageInfo[i].copy()
+
+        img = Image.open(img_path + r'/' + info_tmp['file_name'])
+
+        info_tmp['file_name'] = str(i + img_nums) + '.jpg'
+
+        img_arr = np.array(img)
+        img_arr = img_arr[::-1, :, :]
+        img_v = Image.fromarray(img_arr)
+        img_v.save(img_path + '/' + info_tmp['file_name'], quality=95)
+
+        img_id = info_tmp['id']
+        w, h = info_tmp['width'], info_tmp['height']
+
+        info_tmp['id'] = info_tmp['id'] + img_nums
+        imageInfo.append(info_tmp)
+
+        while idx < annos_nums:
+            if imageAnnos[idx]['image_id'] != img_id: break
+            anno_tmp = imageAnnos[idx].copy()
+            anno_tmp['id'] = anno_tmp['id'] + annos_nums
+            anno_tmp['image_id'] = info_tmp['id']
+
+            for rect in anno_tmp['minAreaRect']:
+                rect[1] = h - rect[1]
+
+            anno_tmp['bbox'][1] = h - anno_tmp['bbox'][1] - anno_tmp['bbox'][3]
+            for seg_idx in range(1, len(anno_tmp['segmentation'][0]), 2):
+                anno_tmp['segmentation'][0][seg_idx] = w - anno_tmp['segmentation'][0][seg_idx]
+
+            imageAnnos.append(anno_tmp)
+            idx = idx + 1
+
+    with open(os.path.join(path, 'new_anno.json'), 'w') as f:
+        annos['images'] = imageInfo
+        annos['annotations'] = imageAnnos
+        json.dump(annos, f, indent=4, sort_keys=True)
+    f.close()
+
+
+if __name__ == '__main__':
+    ImgVertical()
